7a43cde75f252444d53c002da169ff4dcc24f988
kodaikumatani <koudai.aco5423@gmail.com>
2023-07-23 18:54:59 +0900

    chore: CIツールの導入

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,8 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="ja">
+    <Html lang='ja'>
       <Head />
       <body>
         <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { NextPage } from "next";
+import { NextPage } from 'next';
 
 const Index: NextPage = () => {
   return <p>Hello World</p>;
